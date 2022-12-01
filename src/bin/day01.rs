9aa9929300d2a227c0c8d1b63d6690b31bc60041
Rafał Klusek <klusek.raf@gmail.com>
2022-12-01 10:50:31 +0100

    day01 part2

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -9,6 +9,11 @@ fn main() {
     let part1_ans = part1(&input);
     println!("Part 1 time: {:.2?}", part1_start.elapsed());
     println!("Part 1 ans: {:?}", part1_ans);
+
+    let part2_start = Instant::now();
+    let part2_ans = part2(&input);
+    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    println!("Part 2 ans: {:?}", part2_ans);
 }
 
 fn part1(input: &str) -> i32 {
@@ -22,4 +27,24 @@ fn part1(input: &str) -> i32 {
         })
         .max()
         .unwrap()
+}
+
+fn part2(input: &str) -> i32 {
+    let mut elves: Vec<i32> = input.split("\n\n")
+        .into_iter()
+        .map(|elve| {
+            elve.split("\n")
+                .into_iter()
+                .map(|cal| cal.parse::<i32>().unwrap())
+                .sum::<i32>()
+        })
+        .collect();
+    elves.sort();
+    elves.iter()
+        .rev()
+        .take(3)
+        .sum()
+
+
+
 }
