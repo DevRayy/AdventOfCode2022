94bfeb06a805f94dcbcfa3ed03b3a8697f00d13d
Rafał Klusek <klusek.raf@gmail.com>
2022-12-02 10:20:24 +0100

    day02 part 1

---
+++ src/bin/day02.rs
@@ -0,0 +1,38 @@
+use std::fs;
+use std::time::Instant;
+
+fn main() {
+    let input = fs::read_to_string("data/day02.txt")
+        .expect("Unable to load input file");
+
+    let part1_start = Instant::now();
+    let part1_ans = part1(&input);
+    println!("Part 1 time: {:.2?}", part1_start.elapsed());
+    println!("Part 1 ans: {:?}", part1_ans);
+
+    // let part2_start = Instant::now();
+    // let part2_ans = part2(&input);
+    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    // println!("Part 2 ans: {:?}", part2_ans);
+}
+
+fn part1(input: &str) -> i32 {
+    // A, X - rock
+    // B, Y - paper
+    // C, Z - scissors
+    input.split("\n")
+        .into_iter()
+        .map(|x| match x {
+            "A X" => 3 + 1,
+            "A Y" => 6 + 2,
+            "A Z" => 0 + 3,
+            "B X" => 0 + 1,
+            "B Y" => 3 + 2,
+            "B Z" => 6 + 3,
+            "C X" => 6 + 1,
+            "C Y" => 0 + 2,
+            "C Z" => 3 + 3,
+            _ => {println!("{}", x); unreachable!()}
+        })
+        .sum::<i32>()
+}
