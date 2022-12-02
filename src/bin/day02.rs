7548523d9dfda2a99009a4d799ebe74e5e99e38d
Rafał Klusek <klusek.raf@gmail.com>
2022-12-02 10:43:40 +0100

    day02 part 2

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -10,10 +10,36 @@ fn main() {
     println!("Part 1 time: {:.2?}", part1_start.elapsed());
     println!("Part 1 ans: {:?}", part1_ans);
 
-    // let part2_start = Instant::now();
-    // let part2_ans = part2(&input);
-    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
-    // println!("Part 2 ans: {:?}", part2_ans);
+    let part2_start = Instant::now();
+    let part2_ans = part2(&input);
+    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    println!("Part 2 ans: {:?}", part2_ans);
+}
+
+fn part2(input: &str) -> i32 {
+    // A - 1, rock
+    // B - 2, paper
+    // C - 3, scissors
+
+    // X - lose
+    // Y - draw
+    // Z - win
+
+    input.split("\n")
+        .into_iter()
+        .map(|x| match x {
+            "A X" => 0 + 3,
+            "A Y" => 3 + 1,
+            "A Z" => 6 + 2,
+            "B X" => 0 + 1,
+            "B Y" => 3 + 2,
+            "B Z" => 6 + 3,
+            "C X" => 0 + 2,
+            "C Y" => 3 + 3,
+            "C Z" => 6 + 1,
+            _ => {println!("{}", x); unreachable!()}
+        })
+        .sum::<i32>()
 }
 
 fn part1(input: &str) -> i32 {
