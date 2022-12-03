e907b9aa6f975a7e5d5064f5fb34c3cc5b890fb0
Rafał Klusek <klusek.raf@gmail.com>
2022-12-03 12:55:43 +0100

    day03 part2

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -2,6 +2,8 @@ use std::collections::HashSet;
 use std::fs;
 use std::time::Instant;
 
+use itertools::*;
+
 fn main() {
     let input = fs::read_to_string("data/day03.txt")
         .expect("Unable to load input file");
@@ -11,10 +13,10 @@ fn main() {
     println!("Part 1 time: {:.2?}", part1_start.elapsed());
     println!("Part 1 ans: {:?}", part1_ans);
 
-    // let part2_start = Instant::now();
-    // let part2_ans = part2(&input);
-    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
-    // println!("Part 2 ans: {:?}", part2_ans);
+    let part2_start = Instant::now();
+    let part2_ans = part2(&input);
+    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    println!("Part 2 ans: {:?}", part2_ans);
 }
 
 fn part1(input: &str) -> i32 {
@@ -36,6 +38,31 @@ fn part1(input: &str) -> i32 {
                 .nth(0)
                 .unwrap()
                 .clone()
+        })
+        .map(|c| {
+            if c.is_ascii_uppercase() {
+                c as i32 - 38
+            } else {
+                c as i32 - 96
+            }
+        })
+        .sum::<i32>()
+}
+
+fn part2(input: &str) -> i32 {
+    input.split("\n")
+        .into_iter()
+        .tuples()
+        .map(|(elf1, elf2, elf3)| {
+            let ruck1 = elf1.chars().collect::<HashSet<char>>();
+            let ruck2 = elf2.chars().collect::<HashSet<char>>();
+            let ruck3 = elf3.chars().collect::<HashSet<char>>();
+            (ruck1, ruck2, ruck3)
+        })
+        .map(|(ruck1, ruck2, ruck3)| {
+            let mut base = ruck1.clone();
+            base.retain(|e| ruck2.contains(e) && ruck3.contains(e));
+            base.iter().nth(0).unwrap().clone()
         })
         .map(|c| {
             if c.is_ascii_uppercase() {
