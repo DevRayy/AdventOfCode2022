71ba90169d6ee616813a367caf7887d0da4dd239
Rafał Klusek <klusek.raf@gmail.com>
2022-12-03 12:25:01 +0100

    day02 cleanup

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -16,6 +16,27 @@ fn main() {
     println!("Part 2 ans: {:?}", part2_ans);
 }
 
+fn part1(input: &str) -> i32 {
+    // A, X - rock
+    // B, Y - paper
+    // C, Z - scissors
+    input.split("\n")
+        .into_iter()
+        .map(|x| match x {
+            "A X" => 3 + 1,
+            "A Y" => 6 + 2,
+            "A Z" => 0 + 3,
+            "B X" => 0 + 1,
+            "B Y" => 3 + 2,
+            "B Z" => 6 + 3,
+            "C X" => 6 + 1,
+            "C Y" => 0 + 2,
+            "C Z" => 3 + 3,
+            _ => {println!("{}", x); unreachable!()}
+        })
+        .sum::<i32>()
+}
+
 fn part2(input: &str) -> i32 {
     // A - 1, rock
     // B - 2, paper
@@ -37,27 +58,6 @@ fn part2(input: &str) -> i32 {
             "C X" => 0 + 2,
             "C Y" => 3 + 3,
             "C Z" => 6 + 1,
-            _ => {println!("{}", x); unreachable!()}
-        })
-        .sum::<i32>()
-}
-
-fn part1(input: &str) -> i32 {
-    // A, X - rock
-    // B, Y - paper
-    // C, Z - scissors
-    input.split("\n")
-        .into_iter()
-        .map(|x| match x {
-            "A X" => 3 + 1,
-            "A Y" => 6 + 2,
-            "A Z" => 0 + 3,
-            "B X" => 0 + 1,
-            "B Y" => 3 + 2,
-            "B Z" => 6 + 3,
-            "C X" => 6 + 1,
-            "C Y" => 0 + 2,
-            "C Z" => 3 + 3,
             _ => {println!("{}", x); unreachable!()}
         })
         .sum::<i32>()
