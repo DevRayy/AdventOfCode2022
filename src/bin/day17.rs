b2519d7e3200e06aa1a4250ecb7e811127a7f5fe
Rafał Klusek <klusek.raf@gmail.com>
2022-12-17 21:21:47 +0100

    day17 refactor

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -1,4 +1,3 @@
-use std::cmp::{max, min};
 use std::fs;
 use std::time::Instant;
 
@@ -10,190 +9,253 @@ fn main() {
     let part1_ans = part1(&input);
     println!("Part 1 time: {:.2?}", part1_start.elapsed());
     println!("Part 1 ans : {}", part1_ans);
+    assert_eq!(part1_ans, 3048);
 
-    // let part2_start = Instant::now();
-    // let part2_ans = part2(&input);
-    // println!("Part 2 time: {:.2?}", part2_start.elapsed());
-    // println!("Part 2 ans : {:.2?}", part2_ans);
+    let part2_start = Instant::now();
+    let part2_ans = part2(&input);
+    println!("Part 2 time: {:.2?}", part2_start.elapsed());
+    println!("Part 2 ans : {:.2?}", part2_ans);
+    assert_eq!(part2_ans, 1504093567249);
 }
 
-#[derive(Copy, Clone)]
-struct BrickTemplate {
-    len: usize,
-    top: [usize; 4],
-    bottom: [usize; 4],
+#[derive(Eq, PartialEq, Debug, Copy, Clone)]
+struct Point {
+    x: i64,
+    y: i64
 }
 
-struct Brick<'a> {
-    template: &'a BrickTemplate,
-    pos_x: usize,
-    pos_y: usize,
+impl Point {
+    fn new(x: i64, y: i64) -> Point {
+        Point{x, y}
+    }
+
+    fn add(&mut self, p: &Point) {
+        self.x += p.x;
+        self.y += p.y;
+    }
 }
 
-impl <'a>Brick<'a> {
-    fn new(t :&BrickTemplate, x: usize, y: usize) -> Brick {
-        Brick{
+struct Board {
+    inner: Vec<Point>
+}
+
+impl Board {
+    fn new() -> Board {
+        Board{
+            inner: vec![
+                Point::new(0, 0),
+                Point::new(1, 0),
+                Point::new(2, 0),
+                Point::new(3, 0),
+                Point::new(4, 0),
+                Point::new(5, 0),
+                Point::new(6, 0),
+            ]
+        }
+    }
+
+    fn max_height(&self) -> usize {
+        self.inner.iter()
+            .map(|p| p.y)
+            .max()
+            .unwrap_or(0) as usize
+    }
+
+    fn is_blocked(&self, test: &Vec<Point>) -> bool {
+        self.inner.iter()
+            .any(|p| test.contains(p))
+    }
+
+    fn materialize(&mut self, b: &mut Vec<Point>) {
+        self.inner.append(b)
+    }
+}
+
+struct Brick {
+    template: Vec<Point>,
+    origin: Point
+}
+
+impl Brick {
+    fn new(t: Vec<Point>, x: usize, y: usize) -> Brick {
+        Brick {
             template: t,
-            pos_x: x,
-            pos_y: y,
+            origin: Point::new(x as i64, y as i64),
         }
     }
 
-    fn push_left(&mut self, board: &[usize; 7]) {
-        if self.pos_x == 0 {
+    fn push_left(&mut self, board: &Board) {
+        if self.origin.x == 0 {
             return
         }
-        let l = self.render_bottom_left();
-        let blocked = board.iter()
-            .enumerate()
-            .any(|(i, y)| *y == l[i]);
-        if !blocked {
-            self.pos_x = max(self.pos_x - 1, 0);
+        let move_vector = Point::new(-1, 0);
+        let moved = self.moved_by(move_vector);
+        let move_vector = Point::new(-1, 0);
+        if !board.is_blocked(&moved) {
+            self.origin.add(&move_vector);
         }
     }
 
-    fn push_right(&mut self, board: &[usize; 7]) {
-        if self.pos_x == 7 - self.template.len {
+    fn push_right(&mut self, board: &Board) {
+        let template_len = self.template.iter()
+            .map(|p| p.x)
+            .max()
+            .unwrap() + 1;
+        if self.origin.x == 7 - template_len {
             return
         }
-        let r = self.render_bottom_right();
-        let blocked = board.iter()
-            .enumerate()
-            .any(|(i, y)| *y == r[i]);
-        if !blocked {
-            self.pos_x = min(self.pos_x + 1, 7 - self.template.len);
+        let move_vector = Point::new(1, 0);
+        let moved = self.moved_by(move_vector);
+        let move_vector = Point::new(1, 0);
+        if !board.is_blocked(&moved) {
+            self.origin.add(&move_vector);
         }
     }
 
     fn fall(&mut self) {
-        self.pos_y -= 1;
+        let move_vector = Point::new(0, -1);
+        self.origin.add(&move_vector);
     }
 
-    fn top(&self) -> [Option<usize>; 7] {
-        let mut top: [Option<usize>; 7] = [None, None, None, None, None, None, None];
-        for x in 0..self.template.len {
-            top[x + self.pos_x] = Some(self.template.top[x]);
-        }
-        for x in 0..top.len() {
-            match top[x] {
-                None => {},
-                Some(y) => { top[x] = Some(y + self.pos_y)}
-            }
-        }
-
-        top
-    }
-
-    fn render_bottom(&self) -> [usize; 7] {
-        let mut bottom: [usize; 7] = [9, 9, 9, 9, 9, 9, 9];
-        for x in 0..self.template.len {
-            bottom[x + self.pos_x] = self.template.bottom[x];
-        }
-        for x in 0..bottom.len() {
-            bottom[x] += self.pos_y;
-        }
-
-        bottom
-    }
-
-    fn render_bottom_right(&self) -> [usize; 7] {
-        let mut bottom: [usize; 7] = [9, 9, 9, 9, 9, 9, 9];
-        for x in 0..self.template.len {
-            bottom[x + self.pos_x + 1] = self.template.bottom[x];
-        }
-        for x in 0..bottom.len() {
-            bottom[x] += self.pos_y;
-        }
-
-        bottom
-    }
-
-    fn render_bottom_left(&self) -> [usize; 7] {
-        let mut bottom: [usize; 7] = [9, 9, 9, 9, 9, 9, 9];
-        for x in 0..self.template.len {
-            bottom[x + self.pos_x - 1] = self.template.bottom[x];
-        }
-        for x in 0..bottom.len() {
-            bottom[x] += self.pos_y;
-        }
-
-        bottom
-    }
-
-    fn render_next_bottom(&self) -> [usize; 7] {
-        let mut bottom: [usize; 7] = [9, 9, 9, 9, 9, 9, 9];
-        for x in 0..self.template.len {
-            bottom[x + self.pos_x] = self.template.bottom[x];
-        }
-        for x in 0..bottom.len() {
-            bottom[x] += (self.pos_y - 1);
-        }
-
-        bottom
+    fn moved_by(&self, v: Point) -> Vec<Point> {
+        self.template.iter()
+            .map(|p| Point::new(p.x + v.x + self.origin.x, p.y + v.y + self.origin.y))
+            .collect()
     }
 }
 
 fn part1(input: &str) -> usize {
     let valves = input.trim_end().chars().collect::<Vec<char>>();
-    let mut board: [usize; 7] = [0, 0, 0, 0, 0, 0, 0];
-    let bricks: [BrickTemplate; 5] = [
-      BrickTemplate {len: 4, top: [0, 0, 0, 0], bottom: [0, 0, 0, 0]},
-      BrickTemplate {len: 3, top: [1, 2, 1, 0], bottom: [1, 0, 1, 0]},
-      BrickTemplate {len: 3, top: [0, 0, 2, 0], bottom: [0, 0, 0, 0]},
-      BrickTemplate {len: 1, top: [3, 0, 0, 0], bottom: [0, 0, 0, 0]},
-      BrickTemplate {len: 2, top: [1, 1, 0, 0], bottom: [0, 0, 0, 0]},
+    let brick_templates: Vec<Vec<Point>> = vec![
+        vec![Point::new(0, 0), Point::new(1, 0), Point::new(2, 0), Point::new(3, 0)],
+        vec![Point::new(1, 0), Point::new(0, 1), Point::new(1, 1), Point::new(2, 1), Point::new(1, 2)],
+        vec![Point::new(0, 0), Point::new(1, 0), Point::new(2, 0), Point::new(2, 1), Point::new(2, 2)],
+        vec![Point::new(0, 0), Point::new(0, 1), Point::new(0, 2), Point::new(0, 3)],
+        vec![Point::new(0, 0), Point::new(0, 1), Point::new(1, 0), Point::new(1, 1)],
     ];
 
+    solve_for_max_bricks(&valves, &brick_templates, 2022)
+}
+
+fn part2(input: &str) -> usize {
+    let valves = input.trim_end().chars().collect::<Vec<char>>();
+    let brick_templates: Vec<Vec<Point>> = vec![
+        vec![Point::new(0, 0), Point::new(1, 0), Point::new(2, 0), Point::new(3, 0)],
+        vec![Point::new(1, 0), Point::new(0, 1), Point::new(1, 1), Point::new(2, 1), Point::new(1, 2)],
+        vec![Point::new(0, 0), Point::new(1, 0), Point::new(2, 0), Point::new(2, 1), Point::new(2, 2)],
+        vec![Point::new(0, 0), Point::new(0, 1), Point::new(0, 2), Point::new(0, 3)],
+        vec![Point::new(0, 0), Point::new(0, 1), Point::new(1, 0), Point::new(1, 1)],
+    ];
+
+    let target_brick_count = 1000000000000 as usize;
+    let checkpoint = valves.len();
+
+    let (brick_count1, brick_count2, height1, height2) = solve_for_checkpoint(&valves, &brick_templates, checkpoint);
+
+    let brick_count_diff = brick_count2 - brick_count1;
+    let height_diff = height2 - height1;
+
+    let checkpointable_height = (target_brick_count / brick_count_diff) * height_diff;
+
+    let remaining_brick_count = target_brick_count % brick_count_diff;
+    let remainder_height = solve_for_max_bricks(&valves, &brick_templates, remaining_brick_count);
+
+    checkpointable_height + remainder_height
+}
+
+fn solve_for_max_bricks(valves: &Vec<char>, brick_templates: &Vec<Vec<Point>>, max_bricks: usize) -> usize {
+    let mut board = Board::new();
     let mut valve_count: usize = 0;
     let mut brick_count: usize = 0;
 
     let mut brick = Brick::new(
-        &bricks[brick_count % bricks.len()],
+        brick_templates[brick_count % brick_templates.len()].clone(),
         2,
-        board.iter().max().unwrap() + 4
+        board.max_height() + 4
     );
 
     loop {
-        match valves[valve_count % valves.len()] {
-            '<' => { brick.push_left(&board) }
-            '>' => { brick.push_right(&board) }
-            _ => unreachable!()
-        }
-        valve_count += 1;
-
-        //FALL
-        let next_floor = brick.render_next_bottom();
-
-        let blocked = board.iter()
-            .enumerate()
-            .any(|(i, y)| *y == next_floor[i]);
-
-        if !blocked {
-            brick.fall();
-            continue;
-        }
-
-        let top = brick.top();
-
-        for x in 0..board.len() {
-            if let Some(y) = top[x] {
-                board[x] = y;
-            }
-        }
-
-        brick_count += 1;
-        brick = Brick::new(
-            &bricks[brick_count % bricks.len()],
-            2,
-            board.iter().max().unwrap() + 4 //TODO maybe +4?
+        simstep(&valves,
+                &brick_templates,
+                &mut board,
+                &mut valve_count,
+                &mut brick_count,
+                &mut brick
         );
 
-        println!("{:?}", board);
-
-        if brick_count == 2024 {
-            return *board.iter().max().unwrap()
+        if brick_count == max_bricks {
+            return board.max_height()
         }
-
     }
-    unreachable!()
+}
+
+fn solve_for_checkpoint(valves: &Vec<char>, brick_templates: &Vec<Vec<Point>>, checkpoint: usize) -> (usize, usize, usize, usize) {
+    let mut board = Board::new();
+    let mut valve_count: usize = 0;
+    let mut brick_count: usize = 0;
+
+    let mut brick = Brick::new(
+        brick_templates[brick_count % brick_templates.len()].clone(),
+        2,
+        board.max_height() + 4
+    );
+
+    for _ in 0..checkpoint {
+        simstep(&valves,
+                &brick_templates,
+                &mut board,
+                &mut valve_count,
+                &mut brick_count,
+                &mut brick
+        );
+    }
+    let brick_count1 = brick_count;
+    let height1 = board.max_height();
+
+    for _ in 0..checkpoint {
+        simstep(&valves,
+                &brick_templates,
+                &mut board,
+                &mut valve_count,
+                &mut brick_count,
+                &mut brick
+        );
+    }
+    let brick_count2 = brick_count;
+    let height2 = board.max_height();
+    return (brick_count1, brick_count2, height1, height2)
+}
+
+fn simstep(valves: &Vec<char>,
+           brick_templates: &&Vec<Vec<Point>>,
+           board: &mut Board,
+           valve_count: &mut usize,
+           brick_count: &mut usize,
+           brick: &mut Brick
+) {
+    let valve = valves[*valve_count % valves.len()];
+    *valve_count += 1;
+
+    match valve {
+        '<' => { brick.push_left(&board) }
+        '>' => { brick.push_right(&board) }
+        _ => unreachable!()
+    }
+
+    let next_positions = brick.moved_by(Point::new(0, -1));
+    let blocked = board.is_blocked(&next_positions);
+    if !blocked {
+        brick.fall();
+        return
+    }
+
+    let mut materialized_block = brick.moved_by(Point::new(0, 0));
+    board.materialize(&mut materialized_block);
+
+    *brick_count += 1;
+    *brick = Brick::new(
+        brick_templates[*brick_count % brick_templates.len()].clone(),
+        2,
+        board.max_height() + 4
+    );
 }
