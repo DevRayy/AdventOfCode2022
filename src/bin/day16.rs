56c91b19b573f87cefeb61c65fab581a78e0be0b
Rafał Klusek <klusek.raf@gmail.com>
2022-12-18 02:27:33 +0100

    day16 example working

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use std::fs;
 use std::time::Instant;
 use regex::Regex;
+use itertools::Itertools;
 
 fn main() {
     let input = fs::read_to_string("data/day16.txt")
@@ -20,12 +21,12 @@ fn main() {
 
 #[derive(Debug)]
 struct Valve<'a> {
-    rate: u64,
+    rate: usize,
     neighbours: HashMap<&'a str, usize>,
 }
 
 impl <'a> Valve<'a> {
-    fn new(rate: u64, neighbours_raw: &str) -> Valve {
+    fn new(rate: usize, neighbours_raw: &str) -> Valve {
         Valve{
             rate,
             neighbours: neighbours_raw.split(", ").map(|x| (x, 1)).collect(),
@@ -74,15 +75,75 @@ fn parse(input: &str) -> HashMap<&str, Valve> {
 }
 
 fn part1(input: &str) -> usize {
-    let mut graph = parse(input);
+    let graph = parse(input);
 
-    let graph = graph.iter()
+    let graph = reduce_graph(&graph);
+
+    let budget = 30;
+
+    let destinations = graph.iter()
+        .filter(|(k, v)| **k != "AA")
+        .map(|(k, _)| *k)
+        .collect::<Vec<&str>>();
+
+    let mut scores: Vec<usize> = Vec::new();
+    for no_of_permutations in (0..destinations.len()+1) {
+        println!("{}", no_of_permutations);
+        for combination in destinations.iter().permutations(no_of_permutations) {
+            let cost = calc_cost(&combination, &graph);
+            if cost > budget {
+                continue
+            }
+            let score = evaluate_combination(&combination, &graph);
+            scores.push(score);
+        }
+    }
+
+    *scores.iter().max().unwrap()
+}
+
+fn reduce_graph<'a>(graph: &'a HashMap<&str, Valve>) -> HashMap<&'a str, Valve<'a>> {
+    let mut graph = graph.iter()
         .map(|(id, valve)| (*id, valve.expanded(&graph, *id)))
         .collect::<HashMap<&str, Valve>>();
 
-    for (k, v) in graph {
-        println!("{}: {:?}", k, v);
+    let keys_to_del = graph.iter()
+        .filter(|(id, v)| v.rate == 0 && **id != "AA") //because AA is starting point
+        .map(|(id, _)| *id)
+        .collect::<Vec<&str>>();
+
+    graph.iter_mut()
+        .for_each(|(_, v)| v.neighbours
+            .retain(|id, _| !keys_to_del.contains(id))
+        );
+
+    graph.retain(|id, _| !keys_to_del.contains(id));
+    graph
+}
+
+fn calc_cost(path: &Vec<&&str>, graph: &HashMap<&str, Valve>) -> usize {
+    let mut cost = 0;
+    let mut current_node = "AA";
+
+    for destination in path {
+        cost += 1 + graph.get(current_node).unwrap().neighbours.get(*destination).unwrap();
+        current_node = destination;
     }
 
-    0
+    cost
+}
+
+fn evaluate_combination(path: &Vec<&&str>, graph: &HashMap<&str, Valve>) -> usize {
+    let mut budget: usize = 30;
+    let mut current_node = "AA";
+    let mut score: usize = 0;
+
+    for destination in path {
+        let cost = 1 + graph.get(current_node).unwrap().neighbours.get(*destination).unwrap();
+        current_node = destination;
+        budget -= cost;
+        score += budget * graph.get(*destination).unwrap().rate;
+    }
+
+    score
 }
