243f66bc5639c8190bf28f1baa255405b2e01583
Rafał Klusek <klusek.raf@gmail.com>
2022-12-20 13:58:52 +0100

    day20 ordering

--- src/bin/day20.rs
+++ src/bin/day20.rs
@@ -16,11 +16,10 @@ fn main() {
     // println!("Part 2 ans : {:.2?}", part2_ans);
 }
 
-fn parse(input: &str) -> Vec<(i64, bool)> {
+fn parse(input: &str) -> Vec<i64> {
     input.split("\n")
         .map(|line| {
-            let v = line.parse::<i64>().unwrap();
-            (v, v != 0)
+            line.parse::<i64>().unwrap()
         })
         .collect()
 }
@@ -28,14 +27,42 @@ fn parse(input: &str) -> Vec<(i64, bool)> {
 fn part1(input: &str) -> i64 {
     let mut numbers = parse(input);
 
-    mix(&mut numbers);
-    let zero_pos = numbers.iter().position(|x| x.0 == 0).unwrap();
+    let order = ordering(&numbers);
+    mix(&mut numbers, &order);
+
+    let zero_pos = numbers.iter().position(|&x| x == 0).unwrap();
     [1000, 2000, 3000].iter()
-        .map(|x| numbers[(zero_pos + x) % numbers.len()].0)
+        .map(|&x| numbers[(zero_pos + x) % numbers.len()])
         .sum()
 }
 
-fn mix(numbers: &mut Vec<(i64, bool)>) {
+// fn part2(input: &str) -> i64 {
+//     let mut numbers = parse(input);
+//     let encryption_key = 811589153;
+//     numbers = numbers.into_iter()
+//         .map(|n| (n.0 * encryption_key, n.1))
+//         .collect();
+//
+//     println!("{:?}", numbers);
+//     for _ in 0..2 {
+//         numbers = numbers.into_iter()
+//             .map(|n| (n.0, n.0 != 0))
+//             .collect();
+//         mix(&mut numbers);
+//         println!("{:?}", numbers);
+//     }
+//     let zero_pos = numbers.iter().position(|x| x.0 == 0).unwrap();
+//     [1000, 2000, 3000].iter()
+//         .map(|x| numbers[(zero_pos + x) % numbers.len()].0)
+//         .sum()
+// }
+
+fn ordering(numbers: &Vec<i64>) -> Vec<(usize, usize)> {
+    let mut order: Vec<(usize, usize)> = Vec::new();
+    let mut numbers = numbers.iter()
+        .map(|&n| (n, n!=0))
+        .collect::<Vec<(i64, bool)>>();
+
     loop {
         for i in 0..numbers.len() {
             if numbers[i].1 == false {
@@ -46,10 +73,21 @@ fn mix(numbers: &mut Vec<(i64, bool)>) {
             numbers.remove(i);
             let new_pos = (i as i64 + val).rem_euclid(numbers.len() as i64) as usize;
             numbers.insert(new_pos, (val, false));
+            order.push((i, new_pos));
             break;
         }
         if numbers.iter().filter(|x| x.1 == false).count() == numbers.len() {
             break
         }
+    }
+
+    order
+}
+
+fn mix(numbers: &mut Vec<i64>, order: &Vec<(usize, usize)>) {
+    for (i, j) in order {
+        let val = numbers[*i];
+        numbers.remove(*i);
+        numbers.insert(*j, val);
     }
 }
