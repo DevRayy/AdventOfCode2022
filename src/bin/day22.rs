4d14088e4adc578873f07c0bbe19bdc02392f20d
Rafał Klusek <klusek.raf@gmail.com>
2022-12-23 00:46:57 +0100

    day22 part2

--- src/bin/day22.rs
+++ src/bin/day22.rs
@@ -6,10 +6,10 @@ fn main() {
     let input = fs::read_to_string("data/day22.txt")
         .expect("Unable to load input file");
 
-    // let part1_start = Instant::now();
-    // let part1_ans = part1(&input);
-    // println!("Part 1 time: {:.2?}", part1_start.elapsed());
-    // println!("Part 1 ans : {}", part1_ans);
+    let part1_start = Instant::now();
+    let part1_ans = part1(&input);
+    println!("Part 1 time: {:.2?}", part1_start.elapsed());
+    println!("Part 1 ans : {}", part1_ans);
 
     let part2_start = Instant::now();
     let part2_ans = part2(&input);
@@ -30,6 +30,11 @@ enum Move {
     Rotate(char),
     Nothing
 }
+
+const DIR_UP: (i64, i64) = (-1, 0);
+const DIR_DOWN: (i64, i64) = (1, 0);
+const DIR_LEFT: (i64, i64) = (0, -1);
+const DIR_RIGHT: (i64, i64) = (0, 1);
 
 fn parse(input: &str) -> (HashMap<(i64, i64), Tile>, Vec<Move>) {
     let mut splitted = input.split("\n\n");
@@ -56,7 +61,7 @@ fn parse_map(input: &str) -> HashMap<(i64, i64), Tile> {
 }
 
 fn parse_instructions(input: &str) -> Vec<Move> {
-    let mut numbers = input.clone()
+    let numbers = input.clone()
         .replace("R", " ")
         .replace("L", " ");
     let mut numbers = numbers.split(" ")
@@ -125,64 +130,103 @@ fn part2(input: &str) -> i64 {
     let mut pos = (0 as i64, min_y);
     let mut dir = (0 as i64, 1 as i64);
 
-    // for m in moves {
-    //     match m {
-    //         Move::Forward(x) => {
-    //             for _ in 0..x {
-    //                 let new_pos = wrap(&map, &pos, &dir);
-    //
-    //                 if *map.get(&new_pos).unwrap() == Tile::Wall {
-    //                     break
-    //                 }
-    //                 pos = new_pos;
-    //             }
-    //         }
-    //         Move::Rotate(d) => { match d {
-    //             'R' => dir = (dir.1, -dir.0),
-    //             'L' => dir = (-dir.1, dir.0),
-    //             _ => unreachable!()
-    //         }}
-    //         Move::Nothing => unreachable!()
-    //     }
-    // }
+    for m in moves {
+        match m {
+            Move::Forward(x) => {
+                for _ in 0..x {
+                    let (new_dir, new_pos) = wrap3d(&map, &pos, &dir);
+
+                    if *map.get(&new_pos).unwrap() == Tile::Wall {
+                        break
+                    }
+                    pos = new_pos;
+                    dir = new_dir;
+                }
+            }
+            Move::Rotate(d) => { match d {
+                'R' => dir = (dir.1, -dir.0),
+                'L' => dir = (-dir.1, dir.0),
+                _ => unreachable!()
+            }}
+            Move::Nothing => unreachable!()
+        }
+    }
 
     score(&pos, &dir)
 }
 
-fn wrap(map: &HashMap<(i64, i64), Tile>, pos: &(i64, i64), dir: &(i64, i64)) -> (i64, i64) {
-    let mut new_pos = (pos.0 + dir.0, pos.1 + dir.1);
+fn wrap3d(map: &HashMap<(i64, i64), Tile>, pos: &(i64, i64), dir: &(i64, i64)) -> ((i64, i64), (i64, i64)) {
+    let new_pos = (pos.0 + dir.0, pos.1 + dir.1);
+    let new_dir = (dir.0, dir.1);
     let block = map.get(&new_pos);
 
-    if block.is_none() {
-        match dir {
-            (1, 0) => {
-                new_pos = *map.iter()
-                    .filter(|(k, _)| k.1 == pos.1)
-                    .min_by(|a, b| a.0.0.cmp(&b.0.0))
-                    .unwrap().0
-            }
-            (-1, 0) => {
-                new_pos = *map.iter()
-                    .filter(|(k, _)| k.1 == pos.1)
-                    .max_by(|a, b| a.0.0.cmp(&b.0.0))
-                    .unwrap().0
-            }
-            (0, 1) => {
-                new_pos = *map.iter()
-                    .filter(|(k, _)| k.0 == pos.0)
-                    .min_by(|a, b| a.0.1.cmp(&b.0.1))
-                    .unwrap().0
-            }
-            (0, -1) => {
-                new_pos = *map.iter()
-                    .filter(|(k, _)| k.0 == pos.0)
-                    .max_by(|a, b| a.0.1.cmp(&b.0.1))
-                    .unwrap().0
-            }
-            _ => {}
-        }
+    if block.is_some() {
+        return (new_dir, new_pos)
     }
-    new_pos
+
+    let face = face_from_point(pos, 50);
+
+    match (face, *dir) {
+        ((0, 1), DIR_UP)    => (DIR_RIGHT, (pos.1 + 100, 0)),
+        ((3, 0), DIR_LEFT)  => (DIR_DOWN, (0, pos.0 - 100)),
+
+        ((0, 1), DIR_LEFT)  => (DIR_RIGHT, (149 - pos.0, 0)),
+        ((2, 0), DIR_LEFT)  => (DIR_RIGHT, (149 - pos.0, 50)),
+
+        ((0, 2), DIR_UP)    => (DIR_UP, (199, pos.1 - 100)),
+        ((3, 0), DIR_DOWN)  => (DIR_DOWN, (0, 100 - pos.1)),
+
+        ((1, 1), DIR_LEFT)  => (DIR_DOWN, (100, pos.0 - 50)),
+        ((2, 0), DIR_UP)    => (DIR_RIGHT, (pos.1 + 50, 50)),
+
+        ((2, 1), DIR_DOWN)  => (DIR_RIGHT, (pos.1 + 100, 49)),
+        ((3, 0), DIR_RIGHT) => (DIR_UP, (149, pos.0 - 100)),
+
+        ((0, 2), DIR_RIGHT) => (DIR_LEFT, (149 - pos.0, 99)),
+        ((2, 1), DIR_RIGHT) => (DIR_LEFT, (149 - pos.0, 149)),
+
+        ((0, 2), DIR_DOWN)  => (DIR_LEFT, (pos.1 - 50, 99)),
+        ((1, 1), DIR_RIGHT) => (DIR_UP, (49, pos.0 + 50)),
+
+        (_, _) => unreachable!()
+    }
+}
+
+fn wrap(map: &HashMap<(i64, i64), Tile>, pos: &(i64, i64), dir: &(i64, i64)) -> (i64, i64) {
+    let new_pos = (pos.0 + dir.0, pos.1 + dir.1);
+    let block = map.get(&new_pos);
+
+    if block.is_some() {
+        return new_pos;
+    }
+
+    match *dir {
+        DIR_DOWN => {
+            *map.iter()
+                .filter(|(k, _)| k.1 == pos.1)
+                .min_by(|a, b| a.0.0.cmp(&b.0.0))
+                .unwrap().0
+        }
+        DIR_UP => {
+            *map.iter()
+                .filter(|(k, _)| k.1 == pos.1)
+                .max_by(|a, b| a.0.0.cmp(&b.0.0))
+                .unwrap().0
+        }
+        DIR_RIGHT => {
+            *map.iter()
+                .filter(|(k, _)| k.0 == pos.0)
+                .min_by(|a, b| a.0.1.cmp(&b.0.1))
+                .unwrap().0
+        }
+        DIR_LEFT => {
+            *map.iter()
+                .filter(|(k, _)| k.0 == pos.0)
+                .max_by(|a, b| a.0.1.cmp(&b.0.1))
+                .unwrap().0
+        }
+        _ => {unreachable!()}
+    }
 }
 
 fn score(pos: &(i64, i64), dir: &(i64, i64)) -> i64 {
@@ -195,4 +239,8 @@ fn score(pos: &(i64, i64), dir: &(i64, i64)) -> i64 {
     };
 
     1000 * (pos.0 + 1) + 4 * (pos.1 + 1) + dir_score
+}
+
+fn face_from_point(p: &(i64, i64), size: i64) -> (i64, i64) {
+    (p.0 / size, p.1 / size)
 }
